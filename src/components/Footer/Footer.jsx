2e90127242ad774bcd3a579fda5ff1c819109a0a
Dylan Mannan Juarez <dylan_mannan@yahoo.com>
2026-03-17 12:17:39 -0700

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+    <footer>
+        <p className="footer__text">
+            <div className="footer__name">Developed by Dylan Mannan</div>
+            <div className="footer__date">2026</div>
+        </p>
+    </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header({ handleAddClick, weatherData }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="head__date-and-loaction">
+        {currentDate}, {weatherData.city}
+      </p>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add clothes
+      </button>
+      <div className="header__user-container">
+        <p className="header__username">Terrance Tegegne</p>
+        <img src={avatar} alt="Terrance Tegegene" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+export default Header;
